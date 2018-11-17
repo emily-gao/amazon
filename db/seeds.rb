b989c02163dbb52c25dceafec8ce2f16c4404159
Emily Gao <emily.s.gao@gmail.com>
2018-11-17 10:58:59 -0700

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times { Department.create name: Faker::Commerce.unique.department(1) }
+
+5.times do
+  Promotion.create(
+    code: Faker::Commerce.unique.promotion_code(digits = 2),
+    discount: Faker::Number.unique.decimal(0, 2)
+  )
+end
+
+50.times do
+  Product.create(
+    name: Faker::Commerce.unique.product_name,
+    price: Faker::Commerce.price,
+    department: Department.find(Department.ids.shuffle.first),
+    promotion: Promotion.find(Promotion.ids.shuffle.first)
+  )
+end
