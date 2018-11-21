f5f3994b1ad2af50c5aa128e77b3340aa9422cde
Emily Gao <emily.s.gao@gmail.com>
2018-11-20 20:39:58 -0700

    belongs_to doesn't require

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   validates :name, :price, :department, presence: true
 
   belongs_to :department
-  belongs_to :promotion, optional: true
+  belongs_to :promotion
 
   def discounted_price
     return nil unless promotion
